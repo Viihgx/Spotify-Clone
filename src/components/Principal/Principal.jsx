e196aa9ce4937c4889f7aa061ad52d5b2f49c0b4
Viihgx <vitoriasamara.g77@gmail.com>
2023-09-18 14:24:11 -0300

    Iniciais alterações do scroll

--- src/components/Principal/Principal.jsx
+++ src/components/Principal/Principal.jsx
@@ -1,11 +1,12 @@
 import { Outlet } from "react-router-dom";
 import Sidebar from "../SideBar/Sidebar";
+import StickyNav from "../StickyNav";
 
 export default function Principal() {
     return (
         <>
+            <StickyNav />
             <Sidebar />
-
             <Outlet />
         </>
     )
