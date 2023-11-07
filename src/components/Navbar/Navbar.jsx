168658f5a9242ab991857d8bf62f4a58419de5b5
Bryanlimaz <bryandslima@gmail.com>
2023-11-07 14:25:39 -0300

    Profile dropdown and minor settings

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { AppBar, Toolbar, IconButton, Button, Box } from '@mui/material';
 import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
 import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 import ArrowCircleDownIcon from '@mui/icons-material/ArrowCircleDown';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
 import { Link, useNavigate } from 'react-router-dom';
-
 function Navbar() {
   const navigate = useNavigate();
 
@@ -13,8 +14,22 @@ function Navbar() {
     navigate(-1); // Navegue de volta para a página anterior
   };
 
+
+  const [anchorEl, setAnchorEl] = useState(null);
+  const [open, setOpen] = useState(false);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+    setOpen(false);
+  }
+
   return (
-    <AppBar position="fixed" sx={{display: 'flex', width: '74%', marginLeft: 'auto', marginTop: '14px', right: 0, borderRadius: '8px 8px 0 0' , backgroundColor: '#121212', zIndex: '1000'}}>
+    <AppBar position="fixed" sx={{ display: 'flex', width: '71.8%', marginLeft: 'auto', marginTop: '14px', right: 0, borderRadius: '8px 8px 0 0', backgroundColor: '#121212', zIndex: '1000' }}>
       <Toolbar>
         <IconButton
           size="small"
@@ -51,13 +66,13 @@ function Navbar() {
           <Link
             to="/faq"
             style={{ textDecoration: 'none', textTransform: 'none', color: 'white', marginLeft: '10px' }}
-     
+
             rel="noopener noreferrer"
           >
             FAQ
           </Link>
 
-          <div style={{ width: '1px', height: '1.5rem', backgroundColor: 'aliceblue' }} /> 
+          <div style={{ width: '1px', height: '1.5rem', backgroundColor: 'aliceblue' }} />
 
           <Button
             size="small"
@@ -70,6 +85,7 @@ function Navbar() {
               fontWeight: 'bold',
               textTransform: 'none',
               marginLeft: '32rem',
+              lineHeight: '1',
               '&:hover': {
                 backgroundColor: 'white',
                 transform: 'scale(1.05)',
@@ -80,36 +96,70 @@ function Navbar() {
           </Button>
 
           <Button
-              size="small"
-              variant="contained"
-              sx={{
-                borderRadius: '50px',
+            size="small"
+            variant="contained"
+            sx={{
+              borderRadius: '50px',
+              backgroundColor: '#0E0E0E',
+              color: 'white',
+              fontSize: '0.85rem',
+              fontWeight: 'bold',
+              textTransform: 'none',
+              lineHeight: '1',
+              '&:hover': {
                 backgroundColor: '#0E0E0E',
-                color: 'white',
-                fontSize: '0.85rem',
-                fontWeight: 'bold',
-                textTransform: 'none',
-                '&:hover': {
-                  backgroundColor: '#0E0E0E',
-                  transform: 'scale(1.05)',
-                },
-              }}
-            >
-            <ArrowCircleDownIcon style={{marginRight: "0.3rem", fontSize: "21px"}}/> 
+                transform: 'scale(1.05)',
+              },
+            }}
+          >
+            <ArrowCircleDownIcon style={{ marginRight: "0.3rem", fontSize: "1.7rem" }} />
             Instalar aplicativo
           </Button>
 
-          <Button>
-          <AccountCircleOutlinedIcon sx={{ fontSize: '1.8rem', marginRight: '1rem' }} /> 
-          <Link
+          {/* <Link
             to="/Login"
-            style={{ textDecoration: 'none', textTransform: 'none', color: 'white', marginLeft: '10px' }}
-     
+            style={{ marginLeft: '10px' }}
             rel="noopener noreferrer"
-          >
-          Logout
-          </Link>
-          </Button>
+          > */}
+            <Button
+              id="basic-button"
+              aria-controls={open ? 'basic-menu' : undefined}
+              aria-haspopup="true"
+              aria-expanded={open ? 'true' : undefined}
+              onClick={handleClick}
+              sx={{
+                minWidth: '0', padding: '0', display: 'flex', textDecoration: 'none', textTransform: 'none',
+                color: 'white',
+                '&:hover': {
+                  backgroundColor: '#0E0E0E',
+                  transform: 'scale(1.05)',
+                }
+              }}>
+
+
+              <AccountCircleOutlinedIcon />
+
+            </Button>
+            <Menu
+              id="basic-menu"
+              anchorEl={anchorEl}
+              open={open}
+              onClose={handleClose}
+              MenuListProps={{
+                'aria-labelledby': 'basic-button',
+              }}
+            >
+              <Link to="#"
+              style={{textDecoration: 'none', textTransform: 'none', color: 'black'}}>
+              <MenuItem onClick={handleClose}>Meu perfil</MenuItem>
+              </Link>
+              
+              <Link to="/Login"
+              style={{textDecoration: 'none', textTransform: 'none', color: 'black'}}>
+              <MenuItem onClick={handleClose}>Sair</MenuItem>
+              </Link>
+            </Menu>
+          {/* </Link> */}
 
         </Box>
 
